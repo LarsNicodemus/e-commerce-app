9ff2bdfe11ae55fabe0a3821db5c79a0335c4157
LarsNicodemus <lars@no-19.net>
2024-11-18 10:59:43 +0100

    Montag 18.11 Ticket 1.1 Projekt anlegen erledigt

--- Sylando/SylandoApp.swift
+++ Sylando/SylandoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SylandoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ShirtListView()
         }
     }
 }
---
+++ Sylando/Views/ShirtListView.swift
@@ -0,0 +1,18 @@
+//
+//  ShirtListView.swift
+//  Sylando
+//
+//  Created by Lars Nicodemus on 18.11.24.
+//
+
+import SwiftUI
+
+struct ShirtListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShirtListView()
+}
